c44abd178dc8ed0b0094b54f5ec089632a1bbfbf
Eddthing CEO <sangou1270@gmail.com>
2019-09-21 00:11:34 +0900

    1.5

---
+++ routers/globalRouter.js
@@ -0,0 +1,5 @@
+import express from "express";
+
+const globarRouter = express.Router();
+
+export default globarRouter;
---
+++ routers/userRouter.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const userRouter = express.Router();
+
+export default userRouter;
+
+// userRouter.get("/", (req, res) => res.send("user index"));
+// userRouter.get("/edit", (req, res) => res.send("user edit"));
+// userRouter.get("/password", (req, res) => res.send("user password"));
