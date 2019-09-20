b777cba2e552a289e3fc59b13fac2ffaa1718ff2
Eddthing CEO <sangou1270@gmail.com>
2019-09-21 00:29:14 +0900

    1.6

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,5 +1,11 @@
 import express from "express";
-
+import routes from "../routes";
 const globarRouter = express.Router();
+
+globarRouter.get(routes.home, (req, res) => res.send("Home"));
+globarRouter.get(routes.join, (req, res) => res.send("Join"));
+globarRouter.get(routes.login, (req, res) => res.send("Login"));
+globarRouter.get(routes.logout, (req, res) => res.send("Logout"));
+globarRouter.get(routes.search, (req, res) => res.send("Search"));
 
 export default globarRouter;
