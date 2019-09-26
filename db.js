0deef1c83a46f0b93edebe5114ab4920e55201fa
Eddthing CEO <sangou1270@gmail.com>
2019-09-26 21:59:32 +0900

    1.15

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+  {
+    id: 324393,
+    title: "Video super good",
+    description: "This is something I hate",
+    views: 243,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "song",
+      email: "s@d.com"
+    }
+  },
+  {
+    id: 1212121,
+    title: "Video super sad",
+    description: "This is something I miss",
+    views: 124,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "song",
+      email: "s@d.com"
+    }
+  },
+  {
+    id: 55555,
+    title: "Video nice",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "song",
+      email: "s@d.com"
+    }
+  },
+  {
+    id: 11111,
+    title: "Video perfect",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Nicolas",
+      email: "nico@las.com"
+    }
+  }
+];
