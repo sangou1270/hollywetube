131e4e99ed83566a8af63fdb275b6d376d471542
Eddthing CEO <sangou1270@gmail.com>
2019-09-26 22:44:30 +0900

    1.17

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,22 @@
-export const join = (req, res) => res.render("join", { pageTitle: "Join" });
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "Join" });
+};
+
+export const postJoin = (req, res) => {
+  const {
+    body: { name, email, password, password2 }
+  } = req;
+  if (password != password2) {
+    res.status(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    // TO DO : 유저 등록
+    // TO DO : 유저 로그인
+    res.redirect(routes.home);
+  }
+};
 export const login = (req, res) => res.render("login", { pageTitle: "Login" });
 export const logout = (req, res) =>
   res.render("logout", { pageTitle: "Logout" });
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,12 +1,19 @@
 import express from "express";
 import routes from "../routes";
 import { home, search } from "../controllers/videoController";
-import { join, login, logout } from "../controllers/userController";
+import {
+  getJoin,
+  postJoin,
+  login,
+  logout
+} from "../controllers/userController";
 const globarRouter = express.Router();
+
+globarRouter.get(routes.join, getJoin);
+globarRouter.post(routes.join, postJoin);
 
 globarRouter.get(routes.home, home);
 globarRouter.get(routes.search, search);
-globarRouter.get(routes.join, join);
 globarRouter.get(routes.login, login);
 globarRouter.get(routes.logout, logout);
 
