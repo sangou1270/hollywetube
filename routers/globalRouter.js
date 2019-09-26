a60f1f513b32c17f3468af10822e1dd1e0f08c49
Eddthing CEO <sangou1270@gmail.com>
2019-09-26 22:58:12 +0900

    1.18

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -4,17 +4,20 @@ import { home, search } from "../controllers/videoController";
 import {
   getJoin,
   postJoin,
-  login,
-  logout
+  logout,
+  getLogin,
+  postLogin
 } from "../controllers/userController";
 const globarRouter = express.Router();
 
 globarRouter.get(routes.join, getJoin);
 globarRouter.post(routes.join, postJoin);
 
+globarRouter.get(routes.login, getLogin);
+globarRouter.post(routes.login, postLogin);
+
 globarRouter.get(routes.home, home);
 globarRouter.get(routes.search, search);
-globarRouter.get(routes.login, login);
 globarRouter.get(routes.logout, logout);
 
 export default globarRouter;
