01834f5d8ee2fac2c55872367e037e1983f95881
Eddthing CEO <sangou1270@gmail.com>
2019-10-03 20:22:20 +0900

    #3.5 Home Controller Finished

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,14 @@
 import routes from "../routes";
-export const home = (req, res) => {
-  res.render("home", { pageTitle: "Home", videos });
+import Video from "../models/Video";
+
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({}); //db에 존재하는 모든 정보를 가져온다
+    res.render("home", { pageTitle: "Home", videos });
+  } catch (error) {
+    console.log(error);
+    res.render("home", { pageTitle: "Home", videos: [] });
+  }
 };
 
 export const search = (req, res) => {
